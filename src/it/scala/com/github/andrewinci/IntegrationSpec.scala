30f1b583430ac7fcb432f1c92e4aa5f7cbd75919
andrewinci <andrewinci@gmail.com>
2021-06-17 07:17:02 +0100

    feat: Add integration tests

--- src/it/scala/com/github/andrewinci/IntegrationSpec.scala
+++ src/it/scala/com/github/andrewinci/IntegrationSpec.scala
@@ -1,9 +1,20 @@
 package com.github.andrewinci
 
+import com.github.andrewinci.generators.ConstAvroFieldGen
 import munit.FunSuite
+import org.apache.avro.Schema
+
+import scala.io.Source
 
 class IntegrationSpec extends FunSuite {
-//  test("dummy") {
-//    assertEquals(Hello.greeting, "hello")
-//  }
+  test("Test constant avro generator") {
+    // arrange
+    val schema = new Schema.Parser().parse(Source.fromResource("testSchema.avsc").mkString)
+    val fieldGenerator = new ConstAvroFieldGen()
+    val generator = AvroGenerator(fieldGenerator)
+    // act
+    val res = generator.generateRecord(schema)
+    // assert
+    assert(res.isRight)
+  }
 }
