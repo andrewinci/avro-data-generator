4b0cb720d46377dbe9de9d01f0294307c233f782
andrewinci <andrewinci@gmail.com>
2021-06-18 06:00:34 +0100

    feat: Support array in json generator

--- src/main/scala/com/github/andrewinci/generators/JsonAvroFieldGen.scala
+++ src/main/scala/com/github/andrewinci/generators/JsonAvroFieldGen.scala
@@ -14,30 +14,30 @@ class JsonAvroFieldGen(json: JsonNode) extends AvroFieldGenerator {
   def invalidField(field: String) = Left(new FieldGeneratorException(s"Invalid $field type"))
 
   override def getGenerator(fieldName: String): Option[AvroFieldGenerator] = {
-    val field = json.get(fieldName)
-    if (field.isObject) Some(JsonAvroFieldGen(field))
-    else
-      Some(
-        AvroFieldGeneratorLeaf(schema =>
-          schema.getType match {
-            case Type.INT     => Right(field.numberValue().intValue())
-            case Type.LONG    => Right(field.numberValue().longValue())
-            case Type.DOUBLE  => Right(field.numberValue().doubleValue())
-            case Type.FLOAT   => Right(field.numberValue().floatValue())
-            case Type.STRING  => Right(field.textValue())
-            case Type.BOOLEAN => Right(field.booleanValue())
-            case Type.NULL    => Right(null)
-            case Type.ENUM    => avroEnumPicker(schema)(_.find(field.textValue() == _))
-            case Type.BYTES   => unsupportedField(field.toString)
-            case Type.RECORD  => invalidField(field.toString)
-            case Type.ARRAY   => unsupportedField(field.toString)
-            case Type.MAP     => unsupportedField(field.toString)
-            case Type.UNION   => invalidField(field.toString)
-            case Type.FIXED   => unsupportedField(field.toString)
-            case _            => invalidField(field.toString)
-          }
-        )
-      )
+    Option(if (json.isArray) json.get(fieldName.toInt) else json.get(fieldName))
+      .map(field => {
+        if (field.isObject || field.isArray) JsonAvroFieldGen(field)
+        else
+          AvroFieldGeneratorLeaf(schema =>
+            schema.getType match {
+              case Type.INT     => Right(field.numberValue().intValue())
+              case Type.LONG    => Right(field.numberValue().longValue())
+              case Type.DOUBLE  => Right(field.numberValue().doubleValue())
+              case Type.FLOAT   => Right(field.numberValue().floatValue())
+              case Type.STRING  => Right(field.textValue())
+              case Type.BOOLEAN => Right(field.booleanValue())
+              case Type.NULL    => Right(null)
+              case Type.ENUM    => avroEnumPicker(schema)(_.find(field.textValue() == _))
+              case Type.BYTES   => unsupportedField(field.toString)
+              case Type.MAP     => unsupportedField(field.toString)
+              case Type.FIXED   => unsupportedField(field.toString)
+              case Type.RECORD  => invalidField(field.toString)
+              case Type.ARRAY   => invalidField(field.toString)
+              case Type.UNION   => invalidField(field.toString)
+              case _            => invalidField(field.toString)
+            }
+          )
+      })
   }
 
   override def generate(schema: Schema): Either[FieldGeneratorException, Any] =
--- src/test/scala/com/github/andrewinci/generators/JsonAvroFieldGenTest.scala
+++ src/test/scala/com/github/andrewinci/generators/JsonAvroFieldGenTest.scala
@@ -34,9 +34,7 @@ class JsonAvroFieldGenTest extends FunSuite {
                   |""".stripMargin)
 
     // assert
-    val res = gen
-      .map(AvroGenerator(_))
-      .flatMap(_.generateRecord(schema))
+    val res = gen.map(AvroGenerator(_)).flatMap(_.generateRecord(schema))
 
     assertEquals(
       res.map(_.toString),
@@ -48,7 +46,7 @@ class JsonAvroFieldGenTest extends FunSuite {
 
   test("Happy path - fromJson - enum") {
     val sampleSchema =
-      """{"type": "record", "name": "myrec","fields": [
+      """{"type": "record", "name": "myrec", "fields": [
         |{ "name": "test", "type": {
         |  "type": "enum",
         |  "name": "Suit",
@@ -61,21 +59,14 @@ class JsonAvroFieldGenTest extends FunSuite {
     val gen = AvroFieldGen.fromJson("""{"test": "CLUBS" }""")
 
     // assert
-    val res = gen
-      .map(AvroGenerator(_))
-      .flatMap(_.generateRecord(schema))
+    val res = gen.map(AvroGenerator(_)).flatMap(_.generateRecord(schema))
 
-    assertEquals(
-      res.map(_.toString),
-      Right(
-        """{"test": "CLUBS"}"""
-      )
-    )
+    assertEquals(res.map(_.toString), Right("""{"test": "CLUBS"}"""))
   }
 
   test("fromJson - invalid enum") {
     val sampleSchema =
-      """{"type": "record", "name": "myrec","fields": [
+      """{"type": "record", "name": "myrec", "fields": [
         |{ "name": "test", "type": {
         |  "type": "enum",
         |  "name": "Suit",
@@ -88,10 +79,61 @@ class JsonAvroFieldGenTest extends FunSuite {
     val gen = AvroFieldGen.fromJson("""{"test": "TEST" }""")
 
     // assert
-    val res = gen
-      .map(AvroGenerator(_))
-      .flatMap(_.generateRecord(schema))
+    val res = gen.map(AvroGenerator(_)).flatMap(_.generateRecord(schema))
 
     assert(res.isLeft)
+  }
+
+  test("Happy path - fromJson - array") {
+    val sampleSchema =
+      """{"type": "record", "name": "myrec", "fields": [
+        |{ "name": "test", "type": {"type": "array", "items": "string"}}
+        |]}""".stripMargin
+    val schema: Schema = new Schema.Parser().parse(sampleSchema)
+
+    // act
+    val gen = AvroFieldGen.fromJson("""{"test": ["A", "B", "C"]}""")
+
+    // assert
+    val res = gen.map(AvroGenerator(_)).flatMap(_.generateRecord(schema))
+
+    assertEquals(res.map(_.toString), Right("""{"test": ["A", "B", "C"]}"""))
+  }
+
+  test("fromJson - empty array") {
+    val sampleSchema =
+      """{"type": "record", "name": "myrec", "fields": [
+        |{ "name": "test", "type": {"type": "array", "items": "string"}}
+        |]}""".stripMargin
+    val schema: Schema = new Schema.Parser().parse(sampleSchema)
+
+    // act
+    val gen = AvroFieldGen.fromJson("""{"test": []}""")
+
+    // assert
+    val res = gen.map(AvroGenerator(_)).flatMap(_.generateRecord(schema))
+
+    assertEquals(res.map(_.toString), Right("""{"test": []}"""))
+  }
+
+  test("fromJson - array of records") {
+    val sampleSchema =
+      """{"type": "record", "name": "myrec", "fields": [
+        |{ "name": "test", "type": {"type": "array", "items": 
+        |   {"type": "record", "name": "myrec2","fields": [
+        |        { "name": "testString", "type": "string"},
+        |        { "name": "testBoolean", "type": "boolean"}
+        |        ]}
+        |}}
+        |]}""".stripMargin
+    val schema: Schema = new Schema.Parser().parse(sampleSchema)
+
+    // act
+    val gen = AvroFieldGen.fromJson("""{"test": [{"testString": "123", "testBoolean": true}]}""")
+
+    // assert
+    val res = gen.map(AvroGenerator(_)).flatMap(_.generateRecord(schema))
+
+    assertEquals(res.map(_.toString), Right("""{"test": [{"testString": "123", "testBoolean": true}]}"""))
   }
 }
